ccc86d2a6ff8714eb39576d424a0870507951133
preksha3005 <preksha.g504@gmail.com>
2026-01-06 11:26:05 +0530

    Updated code

--- server/index.js
+++ server/index.js
@@ -14,10 +14,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "https://local-store-locator-frontend.onrender.com",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/auth", authRoutes);
 app.use("/stores", storeRoutes);
